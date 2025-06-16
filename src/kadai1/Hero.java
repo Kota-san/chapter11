4a1ee771c929fa3b03373b5a1422ed1a3d4abd99
241019 <241019@std.hi-joho.ac.jp>
2025-06-16 11:50:02 +0900

    ヌッ！

---
+++ src/kadai1/Hero.java
@@ -0,0 +1,7 @@
+package kadai1;
+public class Hero extends Character {
+    @Override
+    public void attack(Matango m) {
+
+    }
+}
---
+++ src/kadai1/Wizard.java
@@ -0,0 +1,6 @@
+package kadai1;
+public class Wizard extends Character {
+    public void attack(Matango m) {
+
+    }
+}
